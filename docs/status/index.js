f837ebc1f924cf1275e20c10a931424fafaeb4b2
buttonbugs <miller.chen2006@outlook.com>
2026-02-14 12:18:39 -0700

    index.js resize body

--- docs/status/index.js
+++ docs/status/index.js
@@ -1,1 +1,3 @@
 document.body.innerHTML = location.search + "<br>" + window.innerWidth + " x " + window.innerHeight
+document.body.style.width = window.innerWidth + "px"
+document.body.style.width = Math.round( window.innerHeight * 1.4) + "px"
