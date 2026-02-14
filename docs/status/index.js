594596f321aa1034adb3e8c784397ab3c42efb92
buttonbugs <miller.chen2006@outlook.com>
2026-02-14 12:23:34 -0700

    Update index.css

--- docs/status/index.js
+++ docs/status/index.js
@@ -1,3 +1,2 @@
 document.body.innerHTML = location.search + "<br>" + window.innerWidth + " x " + window.innerHeight
 document.body.style.width = window.innerWidth + "px"
-document.body.style.height = Math.round( window.innerWidth * 1.4) + "px"
