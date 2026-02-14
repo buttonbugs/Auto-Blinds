41112f1f5639fc2f5a30afdb004e81989937d1ab
buttonbugs <miller.chen2006@outlook.com>
2026-02-14 12:20:47 -0700

    Fix html size bug

--- docs/status/index.js
+++ docs/status/index.js
@@ -1,3 +1,3 @@
 document.body.innerHTML = location.search + "<br>" + window.innerWidth + " x " + window.innerHeight
 document.body.style.width = window.innerWidth + "px"
-document.body.style.width = Math.round( window.innerHeight * 1.4) + "px"
+document.body.style.height = Math.round( window.innerHeight * 1.4) + "px"
